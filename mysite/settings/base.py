22858d334def3ba2390df013ef1310b907dc90e7
Victor <victor.makeaveli@gmail.com>
2021-03-22 11:56:51 -0300

    bug fix

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -18,7 +18,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    
+
     'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'django.contrib.sessions.middleware.SessionMiddleware',
