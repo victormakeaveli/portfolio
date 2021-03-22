173e16806a0b535d057c020d5a27ae80c67bf15e
Victor <victor.makeaveli@gmail.com>
2021-03-22 10:52:51 -0300

    setting deployment

--- mysite/settings/prod.py
+++ mysite/settings/prod.py
@@ -1,1 +1,9 @@
-#
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
